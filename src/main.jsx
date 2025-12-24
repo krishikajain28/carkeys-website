2eb36f41cb2f08bf08a89bfeb63b88b0ecf384a6
Krishika S Jain <krishikajain28@gmail.com>
2025-12-24 12:32:06 +0530

    fixed ts bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'  // <--- THIS IS LIKELY MISSING!
-// import './App.css' // You can delete this if it exists, index.css is what we need
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // <---  for Tailwind
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
